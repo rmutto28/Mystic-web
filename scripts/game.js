2304b1323abece2c670a5b2b40a2434e3f4bb56e
rmutto28 <esfihamutto@gmail.com>
2025-04-23 19:40:29 -0300

    DIP 2 - Raça + Movimento

---
+++ scripts/game.js
@@ -0,0 +1,24 @@
+let canvas = document.getElementById("gameCanvas");
+let ctx = canvas.getContext("2d");
+let x = 50, y = 50;
+
+function initGame() {
+    window.addEventListener("keydown", moveCharacter);
+    draw();
+}
+
+function moveCharacter(e) {
+    switch(e.key) {
+        case "ArrowUp": y -= 10; break;
+        case "ArrowDown": y += 10; break;
+        case "ArrowLeft": x -= 10; break;
+        case "ArrowRight": x += 10; break;
+    }
+    draw();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#0f0";
+    ctx.fillRect(x, y, 30, 30);
+}
