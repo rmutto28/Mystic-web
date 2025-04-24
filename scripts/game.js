281a737e4b5545b40ebeea9bc3be3e16bcb82c7a
rmutto28 <esfihamutto@gmail.com>
2025-04-23 22:24:12 -0300

    DIP FINAL - Mishik RPG funcionando com personagem fluindo

--- scripts/game.js
+++ scripts/game.js
@@ -7,25 +7,25 @@ let personagem = new Image();
 personagem.src = "images/char_humano.png";
 
 function initGame() {
-  window.addEventListener("keydown", moveCharacter);
-  personagem.onload = () => {
-    draw();
-  };
+    window.addEventListener("keydown", moveCharacter);
+    personagem.onload = () => {
+        draw();
+    };
 }
 
 function moveCharacter(e) {
-  switch (e.key) {
-    case "ArrowUp": y -= 10; break;
-    case "ArrowDown": y += 10; break;
-    case "ArrowLeft": x -= 10; break;
-    case "ArrowRight": x += 10; break;
-  }
-  draw();
+    switch(e.key) {
+        case "ArrowUp": y -= 10; break;
+        case "ArrowDown": y += 10; break;
+        case "ArrowLeft": x -= 10; break;
+        case "ArrowRight": x += 10; break;
+    }
+    draw();
 }
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(personagem, x, y, 50, 50);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(personagem, x, y, 50, 50);
 }
 
 window.onload = initGame;
