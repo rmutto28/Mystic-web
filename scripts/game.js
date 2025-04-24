68d9fa091596efc383f516a508bf5f2e2ea2b980
rmutto28 <esfihamutto@gmail.com>
2025-04-23 22:02:29 -0300

    DIP 3 FINAL - Personagem char_humano.png ativado

--- scripts/game.js
+++ scripts/game.js
@@ -1,26 +1,31 @@
+
 let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext("2d");
+
 let x = 50, y = 50;
+let personagem = new Image();
+personagem.src = "images/char_humano.png";
 
 function initGame() {
-    window.addEventListener("keydown", moveCharacter);
+  window.addEventListener("keydown", moveCharacter);
+  personagem.onload = () => {
     draw();
+  };
 }
 
 function moveCharacter(e) {
-    switch(e.key) {
-        case "ArrowUp": y -= 10; break;
-        case "ArrowDown": y += 10; break;
-        case "ArrowLeft": x -= 10; break;
-        case "ArrowRight": x += 10; break;
-    }
-    draw();
+  switch (e.key) {
+    case "ArrowUp": y -= 10; break;
+    case "ArrowDown": y += 10; break;
+    case "ArrowLeft": x -= 10; break;
+    case "ArrowRight": x += 10; break;
+  }
+  draw();
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#0f0";
-    ctx.fillRect(x, y, 30, 30);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(personagem, x, y, 50, 50);
 }
 
 window.onload = initGame;
